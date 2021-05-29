119e1f28e755b185a69a9d5a9759278170e46412
darksidebug <nobeginmasob@gmail.com>
2021-05-29 09:37:50 +0800

    New activity - Stack

---
+++ StackActivity/StackActivity/StackActivity/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StackActivity
+{
+    public partial class Form1 : Form
+    {
+        Stack<string> stack = new Stack<string>();
+       
+        private int i = 1, a = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void gunaControlBox1_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void gunaButton1_Click(object sender, EventArgs e)
+        {
+            this.initStacks(txtItem.Text);
+            this.displayStackResult();
+        }
+
+        private void initStacks(string item)
+        {
+            stack.Push(item);
+            gunaLabel3.Text = "`Push` inserts an object at the top collection"; 
+        }
+
+        private void displayLinkResult()
+        {
+            gunaDataGridView1.Rows.Clear();
+
+            foreach (var listItem in stack)
+            {
+                gunaDataGridView1.Rows.Add(i, listItem);
+                i++;
+            }
+
+            lblAdditionalInfoTxt.Text = "Item(s) count is " + stack.Count;
+            i = 1;
+        }
+
+        private void displayStackResult()
+        {
+            gunaDataGridView1.Rows.Clear();
+
+            foreach (var listItem in stack)
+            {
+                gunaDataGridView1.Rows.Add(i, listItem);
+                i++;
+            }
+
+            lblAdditionalInfoTxt.Text = "Item(s) count is " + stack.Count;
+            i = 1;
+        }
+
+        private int countLinkItems()
+        {
+            return stack.Count(size => size == txtItem.Text);
+        }
+
+        private int countArrayItems()
+        {
+            return stack.Count(size => size == txtItem.Text);
+        }
+
+        private void gunaButton4_Click(object sender, EventArgs e)
+        {
+            gunaDataGridView1.Rows.Clear();
+        }
+
+        private void gunaButton2_Click(object sender, EventArgs e)
+        {
+            gunaLabel3.Text = "`Peek` returns only the top object collection.";
+            gunaLabel4.Text = "Item peek from collection " + stack.Peek();
+            this.displayStackResult();
+        }
+
+        private void gunaButton3_Click(object sender, EventArgs e)
+        {
+            
+            gunaLabel3.Text = "`Pop` removes and returns at the top object collection.";
+            gunaLabel4.Text = "Item remove from collection " + stack.Pop();
+            this.displayStackResult();
+        }
+    }
+}
---
+++ StackActivity/StackActivity/StackActivity/Stack.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StackActivity
+{
+    internal class MyStack<T>
+    {
+        static readonly int MAX = 1000;
+        int top;
+        string[] stack = new string[MAX];
+
+        bool IsEmpty()
+        {
+            return (top < 0);
+        }
+        public MyStack()
+        {
+            top = -1;
+        }
+        internal bool Push(string data)
+        {
+            if (top >= MAX)
+            {
+                MessageBox.Show("Index were outside the bound of array.", "INDEX EXCEPTION", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            else
+            {
+                stack[++top] = data;
+                return true;
+            }
+        }
+
+        internal string Pop()
+        {
+            if (top < 0)
+            {
+                MessageBox.Show("Index were outside the bound of array.", "INDEX EXCEPTION", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return "0";
+            }
+            else
+            {
+                string value = stack[top--];
+                return value;
+            }
+        }
+
+        internal void Peek()
+        {
+            if (top < 0)
+            {
+                MessageBox.Show("Index were outside the bound of array.", "INDEX EXCEPTION", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            else
+                Console.WriteLine("The topmost element of Stack is : {0}", stack[top]);
+        }
+
+        internal int Count()
+        {
+            int i = 0;
+            for (i = 0; i < MAX; i++)
+            {
+                if (string.IsNullOrEmpty(stack[i]))
+                {
+                    break;
+                }
+            }
+
+            return i;
+        }
+
+        internal void PrintStack()
+        {
+            if (top < 0)
+            {
+                MessageBox.Show("Index were outside the bound of array.", "INDEX EXCEPTION", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Items in the Stack are :");
+                for (int i = top; i >= 0; i--)
+                {
+                    Console.WriteLine(stack[i]);
+                }
+            }
+        }
+    }
+}
